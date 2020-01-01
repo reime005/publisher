f6c0ee133c3ca783c8a62c95431f9c0157d6381b
reime005 <reime005@gmail.com>
2020-01-01 19:36:46 +0000

    Deploying to master from  8770ca585f36ad15f0d50c98ecca745057bac293

---
+++ deployed/precache-manifest.8ef2de1038c9b872a178014a4047e55d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ed5543f1706e1ba5fb8d23ac2cbf418",
+    "url": "/publisher/index.html"
+  },
+  {
+    "revision": "74ce7df03af910cef5da",
+    "url": "/publisher/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "0707cbb1d6b70a064b4f",
+    "url": "/publisher/static/js/2.a7b9c5c6.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/publisher/static/js/2.a7b9c5c6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "74ce7df03af910cef5da",
+    "url": "/publisher/static/js/main.b6d2d782.chunk.js"
+  },
+  {
+    "revision": "54f03e7b5af58efb6668",
+    "url": "/publisher/static/js/runtime-main.47bf912a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/publisher/static/media/logo.5d5d9eef.svg"
+  }
+]);
